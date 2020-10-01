2c22b285322d6dfd9fba036fb1a439495f4dde9f
rinayabe <se140663@yahoo.co.jp>
2020-10-01 12:23:36 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, inclusion: { in: %w(@)}
+  validates :encrypted_password, presence: true, length: { minimum: 6 }, format: { with: /\A[a-z\d]+\z/}, confirmation: true
+  validates :family_name, presence: true, format: { with:/\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/}
+  validates :birthday, presence: true
 
 end
