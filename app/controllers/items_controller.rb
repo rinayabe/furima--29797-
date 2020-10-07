3ae8a6f0680b68895f30923c66763391b37c1ddc
rinayabe <se140663@yahoo.co.jp>
2020-10-07 16:49:40 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   #before_action :move_to_index, except: [:index, :show]
   def index
-    ＃@items = Item.all
+    #@items = Item.all
   end
 
   def new
